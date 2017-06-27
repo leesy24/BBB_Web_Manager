51e33ebc85433938a10afb5dc045627a1373fd4c
leesy24 <leesy24@gmail.com>
2017-06-27 14:33:16 +0900

    Connect to controller of ser2net by telnet protocol and enter to monitor mode by "monitor tcp ipv4,tcp,4001".

--- app/serial_monitor.c
+++ app/serial_monitor.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <termios.h>
+#include <netdb.h>
 #include <fcntl.h>
  
 #define IAC_CMD		0xff	// 255: Interpret as command
@@ -28,11 +28,19 @@
 #define CLIENT_WILL	0xfb	// 251: Sender wants to do something.
 
 #define SB			0xfa 	// 250: Subnegotiation of the indicated option follows.
-#define SE			0xf0 // 240: End of subnegotiation parameters.
+#define SE			0xf0	// 240: End of subnegotiation parameters.
 
-#define CMD_ECHO		1
-#define CMD_WINDOW_SIZE 31
- 
+#define CMD_ECHO	1	// Echo
+#define CMD_SGA		3	// Suppress Go Ahead
+#define CMD_NAWS	31	// Negotiate About Window Size
+
+#define CMD_STAT_NONE	0
+#define CMD_STAT_REPLY	1
+#define CMD_STAT_ACK	2
+
+int CMD_ECHO_STAT = CMD_STAT_NONE;
+int CMD_SGA_STAT = CMD_STAT_NONE;
+
 void negotiate(int sock, unsigned char *buf, int len)
 {
 	int i;
@@ -46,22 +54,55 @@ void negotiate(int sock, unsigned char *buf, int len)
 	printf("\n");
 /**/
 
-	if (buf[1] == SERVER_DO && buf[2] == CMD_WINDOW_SIZE)
+	if (buf[1] == SERVER_WILL)
 	{
-		unsigned char tmp1[3] = {IAC_CMD, CLIENT_WILL, CMD_WINDOW_SIZE};
-		if (send(sock, tmp1, 3 , 0) < 0)
-			exit(1);
+		if(buf[2] == CMD_ECHO)
+		{
+			if(CMD_ECHO_STAT != CMD_STAT_NONE)
+			{
+				CMD_ECHO_STAT = CMD_STAT_ACK;
+				return;
+			}
+			buf[1] = CLIENT_DO;
+			CMD_ECHO_STAT = CMD_STAT_REPLY;
+		}
+		else if(buf[2] == CMD_SGA)
+		{
+			if(CMD_SGA_STAT != CMD_STAT_NONE)
+			{
+				CMD_SGA_STAT = CMD_STAT_ACK;
+				return;
+			}
+			buf[1] = CLIENT_DO;
+			CMD_SGA_STAT = CMD_STAT_REPLY;
+		}
+		else
+		{
+			buf[1] = CLIENT_DO;
+		}
+	}
+	else if (buf[1] == SERVER_DO)
+	{
+		if(buf[2] == CMD_NAWS)
+		{
+			unsigned char tmp1[3] = {IAC_CMD, CLIENT_WILL, CMD_NAWS};
+			if (send(sock, tmp1, 3 , 0) < 0)
+				exit(1);
 
-		unsigned char tmp2[9] = {IAC_CMD, SB, CMD_WINDOW_SIZE, 0, 80, 0, 24, IAC_CMD, SE};
-		if (send(sock, tmp2, 9, 0) < 0)
-			exit(1);
+			unsigned char tmp2[9] = {IAC_CMD, SB, CMD_NAWS, 0, 80, 0, 24, IAC_CMD, SE};
+			if (send(sock, tmp2, 9, 0) < 0)
+				exit(1);
+			return;
+		}
+		else
+		{
+			buf[1] = CLIENT_WONT;
+		}
+	}
+	else if (buf[1] == CLIENT_DONT)
+	{
 		return;
 	}
-
-	if (buf[i] == SERVER_DO)
-		buf[i] = CLIENT_WONT;
-	else if (buf[i] == SERVER_WILL)
-		buf[i] = CLIENT_DO;
 
 /**/
 	printf("=>len=%d:", len);
@@ -72,103 +113,148 @@ void negotiate(int sock, unsigned char *buf, int len)
 	printf("\n");
 /**/
 	if (send(sock, buf, len , 0) < 0)
-	exit(1);
+		exit(1);
 }
  
-#define BUFLEN 20
+#define BUFLEN 1024
 int main(int argc , char *argv[]) {
-    int sock;
-    struct sockaddr_in server;
-    unsigned char buf[BUFLEN + 1];
-    int len;
-    int i;
- 
-    if (argc < 2 || argc > 3)
-	{
-        printf("Usage: %s address [port]\n", argv[0]);
-        return 1;
-    }
-    int port = 23;
-    if (argc == 3)
-        port = atoi(argv[2]);
- 
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-	{
-        perror("Could not create socket. Error");
-        return 1;
-    }
- 
-    server.sin_addr.s_addr = inet_addr(argv[1]);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
- 
-    //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-        perror("connect failed. Error");
-        return 1;
-    }
-    puts("Connected...\n");
+	int sock;
+	struct sockaddr_in server;
+	struct hostent *host;
+	unsigned char buf[BUFLEN];
+	int len;
+	int i;
 
-    struct timeval ts;
-    ts.tv_sec = 1; // 1 second
-    ts.tv_usec = 0;
- 
-    while (1)
+	if (argc < 2 || argc > 3)
 	{
-        // select setup
-        fd_set fds;
-        FD_ZERO(&fds);
-        if (sock != 0)
-            FD_SET(sock, &fds);
-        FD_SET(0, &fds);
+		printf("Usage: %s address [port]\n", argv[0]);
+		return 1;
+	}
+	int port = 23;
+	if (argc == 3)
+	port = atoi(argv[2]);
+
+	//Create socket
+	sock = socket(AF_INET , SOCK_STREAM , 0);
+	if (sock == -1)
+	{
+		perror("Could not create socket. Error");
+		return 1;
+	}
+
+	host = gethostbyname(argv[1]);
+	if(host == NULL)
+	{
+		perror("Could find host. Error");
+		return 1;
+	}
+
+	// copy the network address to sockaddr_in structure
+	//memcpy(&server.sin_addr, host->h_addr, host->h_length);
+	//server.sin_addr.s_addr = *(struct in_addr*)(host->h_addr);
+	server.sin_addr = *(struct in_addr *)host->h_addr;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+
+	//Connect to remote server
+	if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		perror("connect failed. Error");
+		return 1;
+	}
+	puts("Connected...\n");
+
+	struct timeval ts;
+	ts.tv_sec = 1; // 1 second
+	ts.tv_usec = 0;
  
-        // wait for data
-        int nready = select(sock + 1, &fds, (fd_set *) 0, (fd_set *) 0, &ts);
-        if (nready < 0)
+	while (1)
+	{
+		// select setup
+		fd_set fds;
+		FD_ZERO(&fds);
+		if (sock != 0)
+			FD_SET(sock, &fds);
+		FD_SET(0, &fds);
+
+		// wait for data
+		int nready = select(sock + 1, &fds, (fd_set *) 0, (fd_set *) 0, &ts);
+		if (nready < 0)
 		{
-            perror("select. Error");
-            return 1;
-        }
-        else if (nready == 0)
+			perror("select. Error");
+			return 1;
+		}
+		else if (nready == 0)
 		{
-            ts.tv_sec = 1; // 1 second
-            ts.tv_usec = 0;
-        }
-        else if (sock != 0 && FD_ISSET(sock, &fds))
+			ts.tv_sec = 1; // 1 second
+			ts.tv_usec = 0;
+		}
+		else if (sock != 0 && FD_ISSET(sock, &fds))
 		{
-            // start by reading a single byte
-            int rv;
-            if ((rv = recv(sock , buf , 1 , 0)) < 0)
-                return 1;
-            else if (rv == 0)
+			if(	CMD_ECHO_STAT != CMD_STAT_ACK ||
+				CMD_ECHO_STAT != CMD_STAT_ACK)
 			{
-                printf("Connection closed by the remote end\n\r");
-                return 0;
-            }
- 
-            if (buf[0] == IAC_CMD)
-			{
-                // read 2 more bytes
-                len = recv(sock , buf + 1 , 2 , 0);
-                if (len  < 0)
-                    return 1;
-                else if (len == 0)
+				// start by reading a single byte
+				int rv;
+				if ((rv = recv(sock , buf , 1 , 0)) < 0)
+					return 1;
+				else if (rv == 0)
 				{
-                    printf("Connection closed by the remote end\n\r");
-                    return 0;
-                }
-                negotiate(sock, buf, 3);
-            }
-            else
+					printf("Connection closed by the remote end\n\r");
+					return 0;
+				}
+
+				if (buf[0] == IAC_CMD)
+				{
+					// read 2 more bytes
+					len = recv(sock , buf + 1 , 2 , 0);
+					if (len  < 0)
+					{
+						perror("Could not recv. Error");
+						return 1;
+					}
+					else if (len == 0)
+					{
+						printf("Connection closed by the remote end\n\r");
+						return 0;
+					}
+					negotiate(sock, buf, 3);
+					if(	CMD_ECHO_STAT == CMD_STAT_ACK &&
+						CMD_ECHO_STAT == CMD_STAT_ACK)
+					{
+						char cmd[] = "monitor tcp ipv4,tcp,4001\r";
+						//char cmd[] = "help\r";
+						/**/
+						printf(">>>len=%d:", strlen(cmd));
+						for(i = 0; i < strlen(cmd); i ++)
+						{
+							printf("cmd[%d]=0x%02x(%d),", i, cmd[i], cmd[i]);
+						}
+						printf("\n");
+						/**/
+						send(sock, cmd, strlen(cmd) , 0);
+					}
+				}
+			}
+			else
 			{
-                putc(buf[0], stdout);
-                fflush(0);
-            }
-        }
-    }
-    close(sock);
-    return 0;
+				len = recv(sock , buf , BUFLEN - 1 , 0);
+				if(len < 0)
+				{
+					perror("Could not recv. Error");
+					return 1;
+				}
+				else if (len == 0)
+				{
+					perror("Connection closed by the remote end");
+					return 0;
+				}
+				buf[len] = 0;
+				printf("%s", buf);
+				fflush(0);
+			}
+		}
+	}
+	close(sock);
+	return 0;
 }
